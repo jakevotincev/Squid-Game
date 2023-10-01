4df856de007b0c5842cf448d7b6ee55151158f72
Evgenii Votintsev <evotintsev@devexperts.com>
2023-10-01 04:12:02 +0300

    add quiz

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,4 +1,3 @@
-
 insert into account (id, name, role, participates_in_game)
 values (1, 'glavniy', 'GLAVNIY', true)
 on conflict do nothing;
@@ -32,4 +31,16 @@ on conflict do nothing;
 
 insert into account (id, name, role, participates_in_game)
 values (6, 'leha', 'WORKER', true)
+on conflict do nothing;
+
+insert into quiz (id, question, answers, game_id, round_id)
+values (1, 'Как называется еврейский Новый год?', '{"Хнука":false,"Йом Кипур":false,"Кванза":false,"Рош ха-Шана":true}', 1, 1)
+on conflict do nothing;
+
+insert into quiz (id, question, answers, game_id, round_id)
+values (2, 'Сколько синих полос на флаге США?', '{"0":true,"3":false,"6":false,"13":false}', 1, 1)
+on conflict do nothing;
+
+insert into quiz (id, question, answers, game_id, round_id)
+values (3, 'Кто из этих персонажей не дружит с Гарри Поттером?', '{"Драко Малфой":true,"Рон Уизли":false,"Гурмиона Грейнджер":false,"Невил Долгопупс":false}', 1, 1)
 on conflict do nothing;
